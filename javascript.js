a199e81851809755e3b031ff5ad2387dec79093e
Student <226680192+t1ram99q-debug@users.noreply.github.com>
2025-09-02 22:20:14 +0530

    Update index and JS, add styles.css for UI

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 let humanScore = 0;
 let computerScore = 0;
-function getHumanChoice() {
+/*function getHumanChoice() {
   var humanChoice = Number(
     prompt("Type a number from 0 = Rock,1 = Scissor,2 = paper"),
   );
@@ -12,44 +12,43 @@ function getHumanChoice() {
     console.log("paper");
   }
   return humanChoice;
-}
+}*/
 function getComputerChoice() {
   let ComputerChoice = Math.floor(Math.random() * 3);
   if (ComputerChoice == 0) {
-    console.log("Rock");
+    return (ComputerChoice = "Rock");
   } else if (ComputerChoice == 1) {
-    console.log("Scissor");
+    return (ComputerChoice = "Scissor");
   } else {
-    console.log("paper");
+    return (ComputerChoice = "Paper");
+  }
+}
+function playRound(humanChoice, ComputerChoice) {
+  if (ComputerChoice == humanChoice) {
+    document.getElementById("result").textContent = "It is draw";
+  } else if (ComputerChoice == "Rock" && humanChoice == "Scissor") {
+    document.getElementById("result").textContent = "You lose!";
+    computerScore++;
+  } else if (ComputerChoice == "Rock" && humanChoice == "Paper") {
+    document.getElementById("result").textContent = "You won!";
+    humanScore++;
+  } else if (ComputerChoice == "Scissor" && humanChoice == "Rock") {
+    document.getElementById("result").textContent = "You won!";
+    humanScore++;
+  } else if (ComputerChoice == "Scissor" && humanChoice == "Paper") {
+    document.getElementById("result").textContent = "You lose!";
+    computerScore++;
+  } else if (ComputerChoice == "Paper" && humanChoice == "Rock") {
+    document.getElementById("result").textContent = "You lose!";
+    computerScore++;
+  } else {
+    document.getElementById("result").textContent = "You won!";
+    humanScore++;
   }
   return ComputerChoice;
 }
-function playRound(humanChoice, computerChoice) {
-  if (computerChoice == humanChoice) {
-    console.log("It is draw");
-  } else if (computerChoice == 0 && humanChoice == 1) {
-    console.log("You lose!");
-    computerScore++;
-  } else if (computerChoice == 0 && humanChoice == 2) {
-    console.log("You won!");
-    humanScore++;
-  } else if (computerChoice == 1 && humanChoice == 0) {
-    console.log("You won!");
-    humanScore++;
-  } else if (computerChoice == 1 && humanChoice == 2) {
-    console.log("You lose!");
-    computerScore++;
-  } else if (computerChoice == 2 && humanChoice == 0) {
-    console.log("You lose!");
-    computerScore++;
-  } else {
-    console.log("You won!");
-    humanScore++;
-  }
-  return computerChoice;
-}
 
-function playGame() {
+/* function playGame() {
   for (let i = 0; i < 5; i++) {
     let humanChoice = getHumanChoice();
     let computerChoice = getComputerChoice();
@@ -70,4 +69,29 @@ function playGame() {
   }
 }
 
-playGame();
+playGame(); */
+
+const buttons = document.querySelectorAll(".buttons button");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const humanChoice = button.id;
+    const computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+    document.getElementById(
+      "choice"
+    ).textContent = `${humanChoice} V/S ${computerChoice}`;
+    document.getElementById("Your-score").textContent =
+      "Your Score: " + humanScore;
+    document.getElementById("Bot-score").textContent =
+      "Bot Score: " + computerScore;
+  });
+});
+const clear = document.getElementById("clear");
+clear.addEventListener("click", () => {
+  humanScore = 0;
+  computerScore = 0;
+  document.getElementById("choice").textContent = "";
+  document.getElementById("result").textContent = "";
+  document.getElementById("Your-score").textContent = "Your Score: 0";
+  document.getElementById("Bot-score").textContent = "Bot Score: 0";
+});
